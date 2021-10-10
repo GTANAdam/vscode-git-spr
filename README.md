29aca633a8c29c0cb0ded4d00bfdaa24cf30a390
Adam <adambh@outlook.com>
2021-10-10 07:54:59 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# VSCode [Git SPR](https://github.com/ejoffe/spr) Integration
+
+The easy gets much easier, VSCode GIT SPR is an extension made to integrate the 'GIT SPR' tool within Visual Studio's Source Control view
+> Project site: [vscode-git-spr](https://github.com/GTANAdam/vscode-git-spr)
+
+![logo](https://i.imgur.com/lFHDnrD.png)
+
+## Getting Started
+
+- Make sure you have configured [GIT SPR](https://github.com/ejoffe/spr) correctly before installing the tool!
+- Make sure to set the correct default terminal profile if your git spr is in a WSL environment.
+
+## Usage
+New navigation buttons menu entries of GIT SPR are generally available anytime the source control view is rendered
+
+## Changelog
+
+**0.0.1** (*10/10/2021*): 
+Initial release 
+
+## Credits
+
+<div>Icon referenced from the <a href="https://github.com/ejoffe/spr" title="GIT SPR Repository">GIT SPR Repository</a>
---
+++ src/index.ts
@@ -0,0 +1,52 @@
+import * as vscode from 'vscode';
+import { GitExtension } from './types/git';
+
+var sprTerminal: vscode.Terminal | null = null;
+const terminalName = "GIT SPR";
+
+export function activate(context: vscode.ExtensionContext) {
+	function getGitExtension() {1
+		const vscodeGit = vscode.extensions.getExtension<GitExtension>('vscode.git');
+		const gitExtension = vscodeGit && vscodeGit.exports;
+		return gitExtension;
+	}
+
+	const gitExtension = getGitExtension();
+	if (!gitExtension?.enabled) {
+		vscode.window.showErrorMessage('Git extensions are not currently enabled, please try again after enabled!');
+		return false;
+	}
+	let repo: any = gitExtension.getAPI(1).repositories[0];
+
+	vscode.window.onDidOpenTerminal((terminal: vscode.Terminal) => {
+		if (terminal.name === terminalName) sprTerminal = terminal
+	});
+
+	vscode.window.onDidCloseTerminal((terminal: vscode.Terminal) => {
+		if (terminal.name === terminalName) sprTerminal = null;
+	});
+
+	context.subscriptions.push(vscode.commands.registerCommand('vscodeGitSpr.Merge', () => {
+		if (sprTerminal === null) sprTerminal = vscode.window.createTerminal(terminalName);
+		sprTerminal.sendText("git-spr merge");
+		sprTerminal.show(true);
+	}));
+
+	context.subscriptions.push(vscode.commands.registerCommand('vscodeGitSpr.Update', () => {
+		if (sprTerminal === null) sprTerminal = vscode.window.createTerminal(terminalName);
+		sprTerminal.sendText("git-spr update");
+		sprTerminal.show(true);
+	}));
+
+	context.subscriptions.push(vscode.commands.registerCommand('vscodeGitSpr.Status', () => {
+		if (sprTerminal === null) sprTerminal = vscode.window.createTerminal(terminalName);
+		sprTerminal.sendText("git-spr status");
+		sprTerminal.show(true);
+	}));
+
+	context.subscriptions.push(vscode.commands.registerCommand('vscodeGitSpr.Amend', () => {
+		if (sprTerminal === null) sprTerminal = vscode.window.createTerminal(terminalName);
+		sprTerminal.sendText("git amend");
+		sprTerminal.show(true);
+	}));
+}
